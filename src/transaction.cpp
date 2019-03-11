0f06153a3f868c95718935305729810a6b0863aa
Leandro Pacheco <leandro.pacheco@galeracluster.com>
2019-03-11 14:49:01 +0100

    use `log_position` when calculating bytes_to_replicate
    `bytes_certified` includes XA events which are not written to the
    binlog. Thus, we need to use `log_position` instead.

--- src/transaction.cpp
+++ src/transaction.cpp
@@ -1148,7 +1148,7 @@ int wsrep::transaction::streaming_step(wsrep::unique_lock<wsrep::mutex>& lock)
 
     int ret(0);
     const ssize_t bytes_to_replicate(client_service_.bytes_generated() -
-                                     streaming_context_.bytes_certified());
+                                     streaming_context_.log_position());
 
     switch (streaming_context_.fragment_unit())
     {
@@ -1697,6 +1697,7 @@ void wsrep::transaction::debug_log_state(
         << ", unit: " << streaming_context_.fragment_unit()
         << ", size: " << streaming_context_.fragment_size()
         << ", counter: " << streaming_context_.unit_counter()
+        << ", log_pos: " << streaming_context_.log_position()
         << ", bytes: " << streaming_context_.bytes_certified()
         << ", sr_rb: " << streaming_context_.rolled_back()
         << "\n    own: " << (client_state_.owning_thread_id_ == wsrep::this_thread::get_id())
